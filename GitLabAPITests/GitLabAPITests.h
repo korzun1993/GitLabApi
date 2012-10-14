4f236ff51b65ba2d7559695fd5beeed7e2c6e07b
korzun1993 <korzun1993@mail.ru>
2012-10-14 11:42:18 +0300

    Initial Commit

---
+++ GitLabAPITests/GitLabAPITests.h
@@ -0,0 +1,13 @@
+//
+//  GitLabAPITests.h
+//  GitLabAPITests
+//
+//  Created by Корзун Владислав on 14.10.12.
+//  Copyright (c) 2012 Корзун Владислав. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GitLabAPITests : SenTestCase
+
+@end
---
+++ GitLabAPITests/GitLabAPITests.m
@@ -0,0 +1,32 @@
+//
+//  GitLabAPITests.m
+//  GitLabAPITests
+//
+//  Created by Корзун Владислав on 14.10.12.
+//  Copyright (c) 2012 Корзун Владислав. All rights reserved.
+//
+
+#import "GitLabAPITests.h"
+
+@implementation GitLabAPITests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GitLabAPITests");
+}
+
+@end
